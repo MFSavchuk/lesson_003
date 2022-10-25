0f21c0ec4c1a1ec845c76013a5671273529a0b08
Mikhail Savchuk <mr.savchuk@bk.ru>
2022-10-25 19:53:40 +0300

    exercise 00, 06, 07, 08 - simple_draw

--- 08_smile.py
+++ 08_smile.py
@@ -17,21 +17,21 @@ def draw_smile(color_element=sd.COLOR_YELLOW):
 
     left_bottom = sd.get_point(x, y)
     right_top = sd.get_point(x + 150, y + 200)
-    sd.ellipse(right_top=right_top, left_bottom=left_bottom, color=color_element, width=1)
+    sd.ellipse(right_top=right_top, left_bottom=left_bottom, color=color_element, width=2)
 
     one = sd.get_point(x + 25, y + 80)
     two = sd.get_point(x + 50, y + 60)
     three = sd.get_point(x + 100, y + 60)
     four = sd.get_point(x + 125, y + 80)
     point_list = [one, two, three, four]
-    sd.lines(point_list=point_list, color=color_element)
+    sd.lines(point_list=point_list, color=color_element, width=2)
 
     center_position1 = sd.get_point(x + 50, y + 125)
     radius = 7
-    sd.circle(center_position=center_position1, radius=radius, color=color_element, width=1)
+    sd.circle(center_position=center_position1, radius=radius, color=color_element, width=2)
 
     center_position2 = sd.get_point(x + 100, y + 125)
-    sd.circle(center_position=center_position2, radius=radius, color=color_element, width=1)
+    sd.circle(center_position=center_position2, radius=radius, color=color_element, width=2)
 
 
 for _ in range(10):
